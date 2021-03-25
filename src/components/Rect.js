7022f89a5e1d765f1ab0435677fadb139df56ab3
Cindy Qian <cindyqian.yushi@gmail.com>
2021-03-24 21:11:26 -0700

    added rectangle features:

--- src/components/Rect.js
+++ src/components/Rect.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './Rect.css';
 
 function Rect(props) {
-  return (
-    <svg className="Rect" width={props.width +"vw"} height={props.height +"%"} 
-    style={{left: props.left + "%" || "0%", right: props.right + "%" || "0%", top: props.top + "%", background: "#" + props.color || "#ffffff", backgroundImage: props.backgroundImage}}/>
-
-  );
+  if(props.type == "round"){
+    return (
+    
+      <svg className="RoundedRect" width={props.width +"vw"} height={props.height +"%"} 
+      style={{left: props.left + "%" || "0%", right: props.right + "%" || "0%", top: props.top + "%", background: "#" + props.color || "#ffffff", backgroundImage: props.backgroundImage}}/>
+      
+    );
+  }else if(props.type == "sharp"){
+    return (
+    
+      <svg className="SharpRect" width={props.width +"vw"} height={props.height +"%"} 
+      style={{left: props.left + "%" || "0%", right: props.right + "%" || "0%", top: props.top + "%", background: "#" + props.color || "#ffffff", backgroundImage: props.backgroundImage}}/>
+      
+    );
+  }
 }
   
 export default Rect;
