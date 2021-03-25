b5d989672f87c2269886378c552698a0dfd6e09a
Cindy Qian <cindyqian.yushi@gmail.com>
2021-03-24 20:17:02 -0700

    updated colors

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ function App() {
 
       <div className="About" >
         <div className="profile-container">
-          <Rect width="20" height="36" top="10" right="5" backgroundImage="linear-gradient(to bottom right, #A5678E, #E8B7D4)"/*color="839A9E"*//>
+          <Rect width="20" height="36" top="10" right="5" /*backgroundImage="linear-gradient(to bottom right, #A5678E, #E8B7D4)"*/color="EB9187"/>
           <Image width="30" height="auto" top="0" right="10" src={ProfileGraphic}/>
           <h1 className="bigHeader" style={{right: "5%" || "0%", top: "60%", color: "#A0B6BA", textAlign: "right"}}>Cindy <br/>Qian</h1>
         </div>
         
 
         <div className="intro-container">
-          <Rect width="25" height="91" left="0" top="0" /*backgroundImage="linear-gradient(to bottom right, #F3A9C3, #FBD489)"*/color="E8B7D4"/>
-          <Rect width="25" height="91" left="3" top="5" /*backgroundImage="linear-gradient(to top left, #F3A9C3, #FBD489)"*/color="A5678E"/>
+          <Rect width="25" height="91" left="0" top="0" /*backgroundImage="linear-gradient(to bottom right, #F3A9C3, #FBD489)"*/color="EB9187"/>
+          <Rect width="25" height="91" left="3" top="5" /*backgroundImage="linear-gradient(to top left, #F3A9C3, #FBD489)"*/color="CB8D9A"/>
           <div className="intro-text-container">
               <p className="normalText" style={{width: "48%", color: "#ffffff", textAlign: "left"}}>
 
@@ -39,7 +39,7 @@ function App() {
                   passions revolve around technology,
                   media, and communications.<br/><br/>
                   
-                  In the future, I hope to find a way to
+                  I hope to find a way to
                   combine my interests to bring people
                   and society together through my work.<br/>
                   I currently plan to major in either CS
@@ -52,6 +52,11 @@ function App() {
 
 
         </div>
+
+        
+
+
+
 
 
 
